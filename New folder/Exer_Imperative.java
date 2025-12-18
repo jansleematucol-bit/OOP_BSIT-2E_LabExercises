458244bb60dfd79568244aa1bc2e07b54438b87b
jansleematucol-bit <janslee.matucol@dorsu.edu.ph>
2025-12-18 12:26:27 +0800

    DIARA SIR THANK YOU

---
+++ New folder/Exer_Imperative.java
@@ -0,0 +1,17 @@
+import java.util.Arrays; 
+
+public class Exer_Imperative {
+    public static void main (String[]  args) {
+        int[] number = {1,2,3,4,5,6,7,8,9,10};
+
+            int sum = 0;
+            for(int i=0; i< number.length; i++){
+                if (number [i] % 2 != 0) {
+                        sum += number[i];
+                }
+            }
+            System.out.println("Imperative Programming");
+
+            System.out.println("Sum of odd numbers = " + sum);
+    }
+}
---
+++ New folder/Shape.java
@@ -0,0 +1,22 @@
+public class Shape{
+      float area;
+
+      public Shape(){
+        this.area = 0.0f; 
+      } 
+
+      public float calculateArea(){
+        return 0.0f;
+      }
+      public String displayInfo(){
+        return null;
+
+      }
+
+      public float getArea(){
+        return this.area;
+      }
+      public void setArea(float area){
+        this.area = area;
+      }
+}
